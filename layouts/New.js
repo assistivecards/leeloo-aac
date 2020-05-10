a23115cf266f35a1990ef26ca8bd87b78898b880
Burak Tokak <iletisim@buraktokak.com>
2020-05-11 01:24:11 +0300

    new profile locale

--- layouts/New.js
+++ layouts/New.js
@@ -47,19 +47,19 @@ export default class Setting extends React.Component {
         <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS == "ios" ? "padding" : "height"}>
           <ScrollView style={{flex: 1, backgroundColor: "#6989FF"}}>
             <View style={styles.head}>
-              <Text style={API.styles.h1}>New Profile</Text>
-              <Text style={API.styles.pHome}>Create a new application user profile.</Text>
+              <Text style={API.styles.h1}>{API.t("settings_selection_new_profile")}</Text>
+              <Text style={API.styles.pHome}>{API.t("settings_new_profile_description")}</Text>
             </View>
             <View style={{flex: 1, backgroundColor: "#fff", borderTopLeftRadius: 30, borderTopRightRadius: 30, paddingTop: 15}}>
               <View style={styles.preferenceItem}>
-                <Text style={API.styles.h3}>Profile Name</Text>
-                <Text style={API.styles.subSmall}>You can give your child's name</Text>
+                <Text style={API.styles.h3}>{API.t("settings_new_profile_profileName_title")}</Text>
+                <Text style={API.styles.subSmall}>{API.t("settings_new_profile_profileName_description")}</Text>
                 <TextInput style={API.styles.input} defaultValue={""} onChangeText={(text) => this.setState({name: text})}/>
               </View>
 
               <View style={styles.preferenceItem}>
-                <Text style={API.styles.h3}>Profile Avatar</Text>
-                <Text style={API.styles.subSmall}>Choose an avatar that looks like the user of this profile.</Text>
+                <Text style={API.styles.h3}>{API.t("settings_new_profile_profileAvatar_title")}</Text>
+                <Text style={API.styles.subSmall}>{API.t("settings_new_profile_profileAvatar_description")}</Text>
                 <View style={{justifyContent: "center", alignItems: "center"}}>
                   <TouchableOpacity style={styles.childAvatar} onPress={() => this.props.navigation.push("Avatar", {avatar: this.changeAvatar.bind(this)})}>
                     <CachedImage uri={`https://leeloo.dreamoriented.org/cdn/avatar/${this.state.avatar}.png?v=${API.version}`} resizeMode="contain" style={styles.childImage} />
@@ -68,8 +68,8 @@ export default class Setting extends React.Component {
               </View>
 
               <View style={styles.preferenceItem}>
-                <Text style={API.styles.h3}>Assistive Cards Packs</Text>
-                <Text style={API.styles.subSmall}>We will set default cards for this profile. You can add or remove the assistive card packs after creating.</Text>
+                <Text style={API.styles.h3}>{API.t("settings_new_profile_aacCards_title")}</Text>
+                <Text style={API.styles.subSmall}>{API.t("settings_new_profile_aacCards_description")}</Text>
               </View>
               <View style={API.styles.iosBottomPadder}></View>
             </View>
