5cfe9ff24727108c64343408163d9254e03efbf6
Burak Tokak <iletisim@buraktokak.com>
2020-09-09 01:45:13 +0300

    rm location package android

--- android/app/src/main/java/org/dreamoriented/leeloo/generated/BasePackageList.java
+++ android/app/src/main/java/org/dreamoriented/leeloo/generated/BasePackageList.java
@@ -21,7 +21,6 @@ public class BasePackageList {
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.localization.LocalizationPackage(),
-        new expo.modules.location.LocationPackage(),
         new expo.modules.notifications.NotificationsPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.screenorientation.ScreenOrientationPackage(),
