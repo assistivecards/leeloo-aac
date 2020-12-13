3cd0718f8edde55bf13af9da23c605ecced1756b
Burak Tokak <iletisim@buraktokak.com>
2020-12-13 19:36:55 +0300

    remove promocode for ios

--- layouts/Subscription.js
+++ layouts/Subscription.js
@@ -161,18 +161,20 @@ export default class Setting extends React.Component {
               }
               {this.state.premium == "lifetime" && <Text style={API.styles.p}>{API.t("settings_subscriptions_downgrade_notice")}</Text>}
               <Text style={API.styles.p}>{API.t("settings_subscriptions_cancel_notice")}</Text>
-              <TouchableOpacity onPress={() => this.redeem()}>
-                <View style={[[styles.selectionItem, {flexDirection: API.user.isRTL ? "row-reverse" : "row"}], {borderBottomWidth: 0, padding: 25, paddingHorizontal: 30}]}>
-                  <Svg height={24} width={24} viewBox="0 0 24 24" style={styles.selectionIcon} strokeWidth="2" stroke="#333" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <Path stroke="none" d="M0 0h24v24H0z"/>
-                    <Line x1="9" y1="15" x2="15" y2="9" />
-                    <Circle cx="9.5" cy="9.5" r=".5" fill="currentColor" />
-                    <Circle cx="14.5" cy="14.5" r=".5" fill="currentColor" />
-                    <Path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7a2.2 2.2 0 0 0 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1a2.2 2.2 0 0 0 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55 v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55 v-1" />
-                  </Svg>
-                  <Text style={[API.styles.b, {fontSize: 15, marginLeft: 10}]}>Redeem Promo Code</Text>
-                </View>
-              </TouchableOpacity>
+              {Platform.OS == "ios" &&
+                <TouchableOpacity onPress={() => this.redeem()}>
+                  <View style={[[styles.selectionItem, {flexDirection: API.user.isRTL ? "row-reverse" : "row"}], {borderBottomWidth: 0, padding: 25, paddingHorizontal: 30}]}>
+                    <Svg height={24} width={24} viewBox="0 0 24 24" style={styles.selectionIcon} strokeWidth="2" stroke="#333" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                      <Path stroke="none" d="M0 0h24v24H0z"/>
+                      <Line x1="9" y1="15" x2="15" y2="9" />
+                      <Circle cx="9.5" cy="9.5" r=".5" fill="currentColor" />
+                      <Circle cx="14.5" cy="14.5" r=".5" fill="currentColor" />
+                      <Path d="M5 7.2a2.2 2.2 0 0 1 2.2 -2.2h1a2.2 2.2 0 0 0 1.55 -.64l.7 -.7a2.2 2.2 0 0 1 3.12 0l.7 .7a2.2 2.2 0 0 0 1.55 .64h1a2.2 2.2 0 0 1 2.2 2.2v1a2.2 2.2 0 0 0 .64 1.55l.7 .7a2.2 2.2 0 0 1 0 3.12l-.7 .7a2.2 2.2 0 0 0 -.64 1.55 v1a2.2 2.2 0 0 1 -2.2 2.2h-1a2.2 2.2 0 0 0 -1.55 .64l-.7 .7a2.2 2.2 0 0 1 -3.12 0l-.7 -.7a2.2 2.2 0 0 0 -1.55 -.64h-1a2.2 2.2 0 0 1 -2.2 -2.2v-1a2.2 2.2 0 0 0 -.64 -1.55l-.7 -.7a2.2 2.2 0 0 1 0 -3.12l.7 -.7a2.2 2.2 0 0 0 .64 -1.55 v-1" />
+                    </Svg>
+                    <Text style={[API.styles.b, {fontSize: 15, marginLeft: 10}]}>Redeem Promo Code</Text>
+                  </View>
+                </TouchableOpacity>
+              }
               <View style={API.styles.iosBottomPadder}></View>
             </View>
           </ScrollView>
