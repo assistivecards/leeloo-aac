a1233ab16bb38a1d3d2b08df02b573805f69df2b
Burak Tokak <iletisim@buraktokak.com>
2021-03-14 13:41:10 +0300

    Better Tablet layout Design

--- layouts/Announcer.js
+++ layouts/Announcer.js
@@ -183,32 +183,34 @@ export default class App extends React.Component {
           <View onLayout={(e) => {
               let {height} = e.nativeEvent.layout;
               this.setState({scrollerHeight: height});
-            }}>
-            <View style={[styles.head, {paddingTop: this.state.orientation == "portrait" ? 100 : 0}]}>
-              <View style={styles.cardHolder}>
-                <TouchableScale onLongPress={() => this.speak(this.card.title)} delayLongPress={16} style={[styles.button, {right: 0}]}>
-                  <Svg viewBox="0 0 24 24" width={32} height={32}>
-                    <Path fill={"#333"} d="M12 4V2.21c0-.45-.54-.67-.85-.35l-2.8 2.79c-.2.2-.2.51 0 .71l2.79 2.79c.32.31.86.09.86-.36V6c3.31 0 6 2.69 6 6 0 .79-.15 1.56-.44 2.25-.15.36-.04.77.23 1.04.51.51 1.37.33 1.64-.34.37-.91.57-1.91.57-2.95 0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-.79.15-1.56.44-2.25.15-.36.04-.77-.23-1.04-.51-.51-1.37-.33-1.64.34C4.2 9.96 4 10.96 4 12c0 4.42 3.58 8 8 8v1.79c0 .45.54.67.85.35l2.79-2.79c.2-.2.2-.51 0-.71l-2.79-2.79c-.31-.31-.85-.09-.85.36V18z"/>
-                  </Svg>
-                </TouchableScale>
-                <TouchableScale style={styles.cardMid} onLongPress={() => this.speak(this.card.title)} delayLongPress={16} >
-                  <Animated.View style={{height: 150, width: 150, borderRadius: 0, overflow: "hidden", backgroundColor: "transparent", padding: 10}}>
-                    <CachedImage uri={`${API.assetEndpoint}cards/${this.pack.slug}/${this.card.slug}.png?v=${API.version}`} style={{width:"100%", height: "100%"}} PlaceholderContent={<ActivityIndicator />} resizeMode="contain" placeholderStyle={{backgroundColor: "#F7F9FB"}}/>
-                  </Animated.View>
-                </TouchableScale>
-                <TouchableScale onLongPress={() => this.speak(this.card.title, "slow")} delayLongPress={16} style={[styles.button, {left: 0}]}>
-                  <Svg viewBox="0 0 24 24" width={32} height={32}>
-                    <Path fill={"#333"} d="M10 8.5v7c0 .41.47.65.8.4l4.67-3.5c.27-.2.27-.6 0-.8L10.8 8.1c-.33-.25-.8-.01-.8.4zm1-5.27c0-.64-.59-1.13-1.21-.99-1.12.26-2.18.7-3.12 1.3-.53.34-.61 1.1-.16 1.55.32.32.83.4 1.21.16.77-.49 1.62-.85 2.54-1.05.44-.1.74-.51.74-.97zM5.1 6.51c-.46-.45-1.21-.38-1.55.16-.6.94-1.04 2-1.3 3.12-.14.62.34 1.21.98 1.21.45 0 .87-.3.96-.74.2-.91.57-1.77 1.05-2.53.26-.39.18-.9-.14-1.22zM3.23 13c-.64 0-1.13.59-.99 1.21.26 1.12.7 2.17 1.3 3.12.34.54 1.1.61 1.55.16.32-.32.4-.83.15-1.21-.49-.76-.85-1.61-1.05-2.53-.09-.45-.5-.75-.96-.75zm3.44 7.45c.95.6 2 1.04 3.12 1.3.62.14 1.21-.35 1.21-.98 0-.45-.3-.87-.74-.96-.91-.2-1.77-.57-2.53-1.05-.39-.24-.89-.17-1.21.16-.46.44-.39 1.19.15 1.53zM22 12c0 4.73-3.3 8.71-7.73 9.74-.62.15-1.22-.34-1.22-.98 0-.46.31-.86.75-.97 3.55-.82 6.2-4 6.2-7.79s-2.65-6.97-6.2-7.79c-.44-.1-.75-.51-.75-.97 0-.64.6-1.13 1.22-.98C18.7 3.29 22 7.27 22 12z"/>
-                  </Svg>
-                </TouchableScale>
+            }} style={{flexDirection: this.state.orientation == "portrait" ? "column" : "row"}}>
+            <View style={this.state.orientation == "portrait" ? {overflow: "hidden"} : {width:  400}}>
+              <View style={[styles.head, {paddingTop: this.state.orientation == "portrait" ? 100 : 50}]}>
+                <View style={styles.cardHolder}>
+                  <TouchableScale onLongPress={() => this.speak(this.card.title)} delayLongPress={16} style={[styles.button, {right: 0}]}>
+                    <Svg viewBox="0 0 24 24" width={32} height={32}>
+                      <Path fill={"#333"} d="M12 4V2.21c0-.45-.54-.67-.85-.35l-2.8 2.79c-.2.2-.2.51 0 .71l2.79 2.79c.32.31.86.09.86-.36V6c3.31 0 6 2.69 6 6 0 .79-.15 1.56-.44 2.25-.15.36-.04.77.23 1.04.51.51 1.37.33 1.64-.34.37-.91.57-1.91.57-2.95 0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-.79.15-1.56.44-2.25.15-.36.04-.77-.23-1.04-.51-.51-1.37-.33-1.64.34C4.2 9.96 4 10.96 4 12c0 4.42 3.58 8 8 8v1.79c0 .45.54.67.85.35l2.79-2.79c.2-.2.2-.51 0-.71l-2.79-2.79c-.31-.31-.85-.09-.85.36V18z"/>
+                    </Svg>
+                  </TouchableScale>
+                  <TouchableScale style={styles.cardMid} onLongPress={() => this.speak(this.card.title)} delayLongPress={16} >
+                    <Animated.View style={{height: 150, width: 150, borderRadius: 0, overflow: "hidden", backgroundColor: "transparent", padding: 10}}>
+                      <CachedImage uri={`${API.assetEndpoint}cards/${this.pack.slug}/${this.card.slug}.png?v=${API.version}`} style={{width:"100%", height: "100%"}} PlaceholderContent={<ActivityIndicator />} resizeMode="contain" placeholderStyle={{backgroundColor: "#F7F9FB"}}/>
+                    </Animated.View>
+                  </TouchableScale>
+                  <TouchableScale onLongPress={() => this.speak(this.card.title, "slow")} delayLongPress={16} style={[styles.button, {left: 0}]}>
+                    <Svg viewBox="0 0 24 24" width={32} height={32}>
+                      <Path fill={"#333"} d="M10 8.5v7c0 .41.47.65.8.4l4.67-3.5c.27-.2.27-.6 0-.8L10.8 8.1c-.33-.25-.8-.01-.8.4zm1-5.27c0-.64-.59-1.13-1.21-.99-1.12.26-2.18.7-3.12 1.3-.53.34-.61 1.1-.16 1.55.32.32.83.4 1.21.16.77-.49 1.62-.85 2.54-1.05.44-.1.74-.51.74-.97zM5.1 6.51c-.46-.45-1.21-.38-1.55.16-.6.94-1.04 2-1.3 3.12-.14.62.34 1.21.98 1.21.45 0 .87-.3.96-.74.2-.91.57-1.77 1.05-2.53.26-.39.18-.9-.14-1.22zM3.23 13c-.64 0-1.13.59-.99 1.21.26 1.12.7 2.17 1.3 3.12.34.54 1.1.61 1.55.16.32-.32.4-.83.15-1.21-.49-.76-.85-1.61-1.05-2.53-.09-.45-.5-.75-.96-.75zm3.44 7.45c.95.6 2 1.04 3.12 1.3.62.14 1.21-.35 1.21-.98 0-.45-.3-.87-.74-.96-.91-.2-1.77-.57-2.53-1.05-.39-.24-.89-.17-1.21.16-.46.44-.39 1.19.15 1.53zM22 12c0 4.73-3.3 8.71-7.73 9.74-.62.15-1.22-.34-1.22-.98 0-.46.31-.86.75-.97 3.55-.82 6.2-4 6.2-7.79s-2.65-6.97-6.2-7.79c-.44-.1-.75-.51-.75-.97 0-.64.6-1.13 1.22-.98C18.7 3.29 22 7.27 22 12z"/>
+                    </Svg>
+                  </TouchableScale>
+                </View>
+              </View>
+
+              <View style={{flexDirection: "column", alignItems: "center"}}>
+                <Text style={[API.styles.h2, {marginTop: 0}]}>{titleCase(this.card.title)}</Text>
+                <Text style={[API.styles.sub, {marginHorizontal: 0, marginBottom: 15}]}>{this.pack.locale}</Text>
               </View>
             </View>
-
-            <View style={{flexDirection: "column", alignItems: "center"}}>
-              <Text style={[API.styles.h2, {marginTop: 0}]}>{titleCase(this.card.title)}</Text>
-              <Text style={[API.styles.sub, {marginHorizontal: 0, marginBottom: 15}]}>{this.pack.locale}</Text>
-            </View>
-            <View style={styles.content}>
+            <View style={[styles.content, {flex: 2, paddingTop: this.state.orientation == "portrait" ? 0 : 50}]}>
               {this.card.phrases.map((phrase, pi) => {
                 return(
                   <TouchableOpacity style={[styles.selectionItem, {flexDirection: API.isRTL() ? "row-reverse" : "row"}]} key={pi} onLongPress={() => this.speak(API.phrase(phrase.phrase))} delayLongPress={16}>
